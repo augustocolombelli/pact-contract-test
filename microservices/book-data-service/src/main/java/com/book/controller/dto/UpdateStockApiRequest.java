695772acab22db676f4f7e6ad263cdf62f006985
Augusto Alessio <augustocolombelli@gmail.com>
2024-04-25 14:24:59 -0300

    Rename quantity field on update stock request

--- microservices/book-data-service/src/main/java/com/book/controller/dto/UpdateStockApiRequest.java
+++ microservices/book-data-service/src/main/java/com/book/controller/dto/UpdateStockApiRequest.java
@@ -9,7 +9,7 @@ public class UpdateStockApiRequest {
     @NotNull(message = "ID has to be present")
     Long id;
 
-    @NotNull(message = "Quantity has to be present")
-    Long quantity;
+    @NotNull(message = "quantityToUpdate has to be present")
+    Long quantityToUpdate;
 
 }
